67d031dd4beddc053752b6a0a43bd3f7a2657297
shiri <shiri957313@gmail.com>
2026-07-24 14:40:32 +0530

    Fix CourseService API

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,8 +1,21 @@
-export const getCourses = () => axios.get(API, authHeader());
+import axios from "axios";
 
-export const getCourse = (id) => axios.get(`${API}/${id}`, authHeader());
+const API = `${import.meta.env.VITE_API_URL}/courses`;
 
-export const addCourse = (course) => axios.post(API, course, authHeader());
+const authHeader = () => ({
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+});
+
+export const getCourses = () =>
+    axios.get(API, authHeader());
+
+export const getCourse = (id) =>
+    axios.get(`${API}/${id}`, authHeader());
+
+export const addCourse = (course) =>
+    axios.post(API, course, authHeader());
 
 export const updateCourse = (id, course) =>
     axios.put(`${API}/${id}`, course, authHeader());
