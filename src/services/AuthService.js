5f663efd71397ca81372875b4204e01aea4f57e8
shiri <shiri957313@gmail.com>
2026-07-25 21:47:39 +0530

    Fixed all frontend API URLs

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "import.meta.env.VITE_API_URL";
+const API_URL = `${import.meta.env.VITE_API_URL}/users`;
 
 export const forgotPassword = (email) => {
     return axios.post(`${BASE_URL}/forgot-password`, {
